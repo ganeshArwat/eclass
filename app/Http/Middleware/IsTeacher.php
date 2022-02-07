859e06c484c3cb12f76f21a88861e1c5ee2e489c
Ganesh Arwat <ganesharwat123@gmail.com>
2022-02-07 21:36:51 +0530

    login commit 3

--- app/Http/Middleware/IsTeacher.php
+++ app/Http/Middleware/IsTeacher.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class IsTeacher
 {
@@ -16,11 +17,19 @@ class IsTeacher
      */
     public function handle(Request $request, Closure $next)
     {
+        if (Auth::check()) {
+            // The user is logged in...
+
+            
         if(auth()->user()->role_id == 2){
             return $next($request);
         }
    
-        return redirect('home')->with('error',"You don't have teacher access.");
+        return redirect('home')->with('error',"You don't have admin access.");
+
+        }else{
+            return redirect('login')->with('error',"You don't have admin access.");
+        }
 
     }
 }
